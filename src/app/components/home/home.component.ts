5710c3485456f65400cb23aa90c29b459103ec39
Agustin Fiori <afiori@clarika.com.ar>
2019-08-17 20:15:01 -0300

    Class 95 - New releases list

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,10 +8,14 @@ import { SpotifyService } from '../../services/spotify.service'
 })
 export class HomeComponent implements OnInit {
 
-  countries: any[] = [];
+  newReleases: any[] = [];
 
   constructor( private spotify: SpotifyService ) { 
-    this.spotify.getNewReleases();
+    this.spotify.getNewReleases()
+      .subscribe( ( data: any ) => {
+        console.log(data.albums.items);
+        this.newReleases = data.albums.items;
+      } );
   }
 
   ngOnInit() {
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,18 +6,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class SpotifyService {
 
-  constructor( private http: HttpClient ) { 
+  constructor( private http: HttpClient ) {
     console.log('spotify service ready');
   }
 
   getNewReleases(){
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQAos5sLip-W1wSkWPloHe_eR7CZDcB3oYdLKfX_sSlH9iAVoASRLIdSvycZdpQa44ml1VH4b_Zw-eMLOXE'
+      'Authorization': 'Bearer BQAt93_AsgqFHG034AvDBTY3BvjH3X9RdYbJIoHzFh8GG0Mn-sIiXLGM8hHGAKolAx6oJ_WQIndj6PLe5bQ'
     });
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-    .subscribe( data => {
-      console.log(data);
-    });
+
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
 
   }
 }
