174a9d6fe094aa9877b2cb2ec8478e17c925741f
Agustin Fiori <afiori@clarika.com.ar>
2019-08-18 20:17:46 -0300

    Class 97 - Map in Observables

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +13,24 @@ export class SpotifyService {
 
   getNewReleases() {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQD3MYDM5aidMf5Zss2n6slLuiDLlQ15SUp94QISnUmUcEzpX94vw9vUTOBSHpcx_CguLWk5KDbot7kUCns'
+      'Authorization': 'Bearer BQA3BJAEmEAmarrHnrjh6UG8sPfW7q4AG8O6RzSxw_jK88IxGIzgSwmy21O6jqj82Lb1T1YhBZKi7W1YQdk'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers });
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+      .pipe( map( (data: any) => {
+        return data.albums.items;
+      } ) );
 
   }
 
   searchArtists( term: string ) {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQD3MYDM5aidMf5Zss2n6slLuiDLlQ15SUp94QISnUmUcEzpX94vw9vUTOBSHpcx_CguLWk5KDbot7kUCns'
+      'Authorization': 'Bearer BQA3BJAEmEAmarrHnrjh6UG8sPfW7q4AG8O6RzSxw_jK88IxGIzgSwmy21O6jqj82Lb1T1YhBZKi7W1YQdk'
     });
 
-    return this.http.get('https://api.spotify.com/v1/search?q=' + term + '&type=artist&limit=20', { headers });
+    return this.http.get('https://api.spotify.com/v1/search?q=' + term + '&type=artist&limit=20', { headers })
+      .pipe( map( (data: any) => {
+        return data.artists.items;
+      } ) );
   }
 }
