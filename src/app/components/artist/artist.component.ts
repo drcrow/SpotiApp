f41d3f4a1d065b343ff623d07189af6e38ca9210
Agustin Fiori <afiori@clarika.com.ar>
2019-08-31 19:29:46 -0300

    Class 102 - Artist component, route

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
   styles: []
 })
-export class ArtistComponent implements OnInit {
+export class ArtistComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor( private myActRoute: ActivatedRoute ) {
+    this.myActRoute.params.subscribe( params => {
+      console.log(params.id);
+    } );
   }
+
+
 
 }
--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cards',
@@ -9,9 +10,26 @@ export class CardsComponent implements OnInit {
 
   @Input() items: any[] = [];
 
-  constructor() { }
+  constructor( private myRouter: Router ) { }
 
   ngOnInit() {
+  }
+
+  // in home this component shows disks so we need to get the first artist to show
+  // in search seaction this component shows artists
+  showFirstArtist( item: any ) {
+    let artistID: string;
+    if ( item.type === 'artist' ) {
+      artistID = item.id;
+    } else {
+      artistID = item.artists[0].id;
+    }
+
+    this.showArtist(artistID);
+  }
+
+  showArtist( id: string ) {
+    this.myRouter.navigate(['/artist', id]);
   }
 
 }
